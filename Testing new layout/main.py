b675b965518d63fc3ee89f10aecd643ec7d06838
Faultier523 <82906372+Faultier523@users.noreply.github.com>
2022-04-13 13:46:26 +0200

    Update main.py

--- Testing new layout/main.py
+++ Testing new layout/main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivymd.app import MDApp
+# from kivymd.app import MDApp
 from pytube import YouTube
 import os
 
