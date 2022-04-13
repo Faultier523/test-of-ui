b0b99bcd8c7917ff2be1ca10f7550323ba4b2049
Sodawasser <103574551+SodawasserHD@users.noreply.github.com>
2022-04-13 13:20:27 +0200

    Add files via upload

---
+++ Testing new layout/main.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from pytube import YouTube
+import os
+
+Builder.load_file("styles.kv")
+
+class MyLayout(Widget):
+    def clear(self):
+        self.ids.textinp.text = ""
+
+class application(App):
+    def build(self):
+        Window.clearcolor = (1, 1, 1, 1) #Hintergrund Window
+        return MyLayout()
+
+application().run()
